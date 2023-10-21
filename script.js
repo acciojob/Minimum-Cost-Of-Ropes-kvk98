4122439f46b13f4fdefd51cbadea7930714ac99f
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-21 11:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
   //your code here
-  
+	let input = document.getElementById("rope-lengths");
+	let arrayString = input.value.split(",");
+	let arrayNum = arrayString.map(Number);
+	let sortedArray = arrayNum.sort((a,b)=>(a-b));
+	let cost = 0;
+	let sum = 0;
+		cost = sortedArray[0] + sortedArray[1] ;
+	for (let i = 2; i <sortedArray.length; i++) {
+		let num = Number(sortedArray[i]);
+		cost = cost + num; 
+	}
+	let result = document.getElementById("result");
+	result.innerHTML = cost;
+	
   
   
 }  
