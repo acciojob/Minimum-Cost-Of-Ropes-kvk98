bc6f6be6a49098d1d0929397a17da39dbd0b5c86
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-11-01 10:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,39 +19,70 @@
   
 // }  
 
-function calculateMinCost() {
-  // Get the input element and its value
-	let form = document.getElementById("form");
-  let input = document.getElementById("rope-lengths");
-  let arrayString = input.value.split(", ");
+// function calculateMinCost() {
+//   // Get the input element and its value
+// 	let form = document.getElementById("form");
+//   let input = document.getElementById("rope-lengths");
+//   let arrayString = input.value.split(", ");
   
-  // Convert the input string into an array of numbers
-  let arrayNum = arrayString.map(Number);
+//   // Convert the input string into an array of numbers
+//   let arrayNum = arrayString.map(Number);
 
-  // Initialize the cost
-  let cost = 0;
+//   // Initialize the cost
+//   let cost = 0;
 
-  // While there are at least two ropes
-  while (arrayNum.length >= 2) {
-    // Sort the array in ascending order
-    arrayNum.sort((a, b) => a - b);
+//   // While there are at least two ropes
+//   while (arrayNum.length >= 2) {
+//     // Sort the array in ascending order
+//     arrayNum.sort((a, b) => a - b);
 
-    // Take the two shortest ropes
-    const min1 = arrayNum.shift();
-    const min2 = arrayNum.shift();
+//     // Take the two shortest ropes
+//     const min1 = arrayNum.shift();
+//     const min2 = arrayNum.shift();
 
-    // Calculate the cost of connecting these two ropes
-    const connectionCost = min1 + min2;
+//     // Calculate the cost of connecting these two ropes
+//     const connectionCost = min1 + min2;
 
-    // Add the connection cost to the total cost
-    cost += connectionCost;
+//     // Add the connection cost to the total cost
+//     cost += connectionCost;
 
-    // Insert the new rope length back into the array
-    arrayNum.push(connectionCost);
-  }
+//     // Insert the new rope length back into the array
+//     arrayNum.push(connectionCost);
+//   }
 
-  // Display the result
-  let result = document.getElementById("result");
-  result.innerHTML = cost;
+//   // Display the result
+//   let result = document.getElementById("result");
+//   result.innerHTML = cost;
+// }
+
+
+
+let input = document.querySelector("input");
+let result = document.querySelector("#result");
+function calculateMinCost() {
+	let inputArray = input.value.split(", ");
+	
+	let sum=0, cost=0;
+
+	while (inputArray.length>0) {
+		inputArray.sort((a,b)=> {
+			return a-b;
+		})
+		if (inputArray.length === 1) {
+			sum = inputArray[0];
+			inputArray.shift();
+			cost += sum;
+			return cost;
+		}
+		else {
+			sum = inputArray[0] + inputArray[1];
+			inputArray.shift();
+			inputArray.shift();
+			inputArray.unshift(sum);
+			cost = cost + sum;
+		}
+	}
+
+	result.innerHTML = cost;
+	// return 
 }
-
